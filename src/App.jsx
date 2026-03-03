1a7e5d85ea82ed27eed543d6faa45c80df0fc9a1
alfazkhan <khanalfaaz14@gmail.com>
2026-03-03 14:10:21 +0100

    Initial structure done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Logo from "./assets/investment-calculator-logo.png"
+import Result from "./components/Result"
+import UserInput from "./components/UserInput"
+
+function App() {
+  return (
+    <>
+    <UserInput/>
+    <Result/>
+    </>
+  )
+}
+
+export default App
